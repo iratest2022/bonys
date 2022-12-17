449a809a9351912b8b74db9a85a328a9d6ef7f47
Irina.Test <ira.test2022@gmail.com>
2022-12-17 19:44:23 +0300

    gitignore

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int score =100;
+        int depositAccount =1100;
+        int bonus;
+
+        if (depositAccount > 1000) {
+            bonus = depositAccount / 100;
+        }
+        else {
+            bonus = 0;
+        }
+
+        int balance = score + depositAccount + bonus;
+        System.out.println("Ваш баланс: " + balance + " руб. Ваш бонус: " + bonus + " руб.");
+    }
+}
